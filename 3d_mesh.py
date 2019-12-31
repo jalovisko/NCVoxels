e16169fcb1e096b09c120ad48f98f3f82a1fafe2
Nikita Letov <letovnn@gmail.com>
2019-12-31 02:00:15 -0500

    Added axes labels to the plot_torus function.

--- 3d_mesh.py
+++ 3d_mesh.py
@@ -58,10 +58,20 @@ def plot_torus(torus):
     ax1.set_zlim(-3,3)
     ax1.plot_surface(x, y, z, rstride=5, cstride=5, color='k', edgecolors='w')
     ax1.view_init(36, 26)
+    
+    ax1.set_xlabel("x")
+    ax1.set_ylabel("y")
+    ax1.set_zlabel("z")
+    
     ax2 = fig.add_subplot(122, projection='3d')
     ax2.set_zlim(-3,3)
     ax2.plot_surface(x, y, z, rstride=5, cstride=5, color='k', edgecolors='w')
     ax2.view_init(0, 0)
+    
+    ax2.set_xlabel("x")
+    ax2.set_ylabel("y")
+    ax2.set_zlabel("z")
+    
     ax2.set_xticks([])
     plt.show()
     
